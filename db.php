0b923b2db73c8ca48ca5c28f1a6982ab0cc02070
chrisbialy <krzysztofbialy@o2.pl>
2018-02-18 12:26:13 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $mysql_hostname = "comp-hons.uhi.ac.uk";
-$mysql_user = "pe14002792";
-$mysql_password = "16051994";
-$mysql_database = "pe14002792";
+$mysql_user = "";
+$mysql_password = "";
+$mysql_database = "";
 $prefix = "";
 $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
 mysql_select_db($mysql_database, $bd) or die("Could not select database");
